4e1389a1d9a39f3a6faac5baa609f7ec63ccebf6
Luan Pham <phamquiluan@gmail.com>
2021-04-07 16:57:56 +0700

    Update merge script to use multiprocessing

--- merge.py
+++ merge.py
@@ -4,7 +4,8 @@ import cv2
 import numpy as np
 
 
-for mask_path in glob.glob("./output_task1/fold1/*"):
+# for mask_path in glob.glob("./output_task1/fold1/*"):
+def process_single(mask_path):
     mask_name = os.path.basename(mask_path)
 
     m1 = cv2.imread(mask_path)
@@ -16,3 +17,9 @@ for mask_path in glob.glob("./output_task1/fold1/*"):
     m = m1 / 5 + m2 / 5 + m3 / 5 + m4 / 5 + m5 / 5
 
     cv2.imwrite(f"./output_task1/merge/{mask_name}", m)
+
+
+from multiprocessing.pool import Pool
+
+with Pool(int(os.cpu_count() * 0.8)) as p:
+    p.map(process_single, glob.glob("./output_task1/fold1/*"))
